54aa915aed03ce1268222dfadb80244a0172c8cf
itz-kunal <kunal.wd067@gmail.com>
2024-06-05 11:55:53 +0530

    vercel.json modify

--- Frontend/src/utils/apiRoutes.js
+++ Frontend/src/utils/apiRoutes.js
@@ -1,5 +1,5 @@
 
-const BASE_URL = 'http://localhost:80';
+const BASE_URL = process.env.API_URI || 'https://genie-ai0-api.vercel.app' || 'http://localhost:80';
 
 export const LOGIN = `${BASE_URL}/api/login`;
 export const SIGNUP = `${BASE_URL}/api/signup`;
