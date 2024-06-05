631c6f589308baa8c4da642b0135c6ae836af599
itz-kunal <kunal.wd067@gmail.com>
2024-06-05 14:18:02 +0530

    lp

--- Backend/app.js
+++ Backend/app.js
@@ -4,18 +4,27 @@ const cookieParser = require('cookie-parser');
 
 const v1Routes = require('./routes/v1.route');
 const v2Routes = require('./routes/v2.route');
-const { decodeToken } = require("./middlewares/auth.middleware");
+const {
+    decodeToken
+} = require("./middlewares/auth.middleware");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}));
 app.use(cookieParser());
 
-app.use(cors({
-    origin: ["http://localhost:3000", "https://genie-ai0.vercel.app", "https://genie-ai0.vercel.app/"],
-    credentials: true
-}));
+app.use((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://genie-ai0.vercel.app');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use(cors());
 
 app.use('/api/v1', v1Routes);
 app.use('/api/v2', decodeToken, v2Routes);
--- Backend/configs/db.config.js
+++ Backend/configs/db.config.js
@@ -1,6 +1,6 @@
 const { default: mongoose } = require("mongoose");
 
-const dbUrl = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/GenieAi';
+const dbUrl = process.env.MONGO_URI || 'mongodb+srv://trekomi:AkqEPipnajKeTrTC@cluster0.uy9ofus.mongodb.net/GenieAi';
 const dbConnect = async () => {
     try {
         await mongoose.connect(dbUrl, {
